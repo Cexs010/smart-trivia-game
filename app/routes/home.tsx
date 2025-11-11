9d7839eff095c14d434ea1c37be5990eeec707a7
cexs <cesar.serafin@outlook.com>
2025-11-10 18:37:17 -0600

    feat(home): mejorar diseño de pantalla principal con nuevo fondo

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -10,12 +10,12 @@ export function meta({ }: Route.MetaArgs) {
 export default function Home() {
   return (
     <main className="flex h-screen bg-(--color-primario) text-white">
-      <div className="w-1/2 bg-[url('./images/Landing-Image.jpg')] bg-cover bg-center rounded-tr-3xl rounded-br-3xl"></div>
+      <div className="w-1/2 bg-[url('./assets/Landing-Image.jpg')] bg-cover bg-center rounded-tr-3xl rounded-br-3xl"></div>
       <div className="w-1/2 flex flex-col gap-y-10 items-center justify-center text-center font-semibold">
-        <span className="text-5xl">¡Bienvenido al juego <br /> de trivia más divertido!</span>
+        <span className="text-5xl tracking-[0.3rem]">¡Bienvenido al juego <br /> de trivia más divertido!</span>
         <button
           type="submit"
-          className="px-6 py-3 text-4xl text-white rounded-lg shadow-md hover:shadow-lg active:shadow-sm active:translate-y-1 transition-all duration-100"
+          className="px-6 w-lg py-3 text-4xl text-white rounded-lg shadow-md hover:shadow-lg active:shadow-sm active:translate-y-1 transition-all duration-100"
           style={{ background: "var(--color-secundario)" }}
         >
           Comenzar
