24e489c9fe85776c1faf28aafa823152ef416a9f
cexs <cesar.serafin@outlook.com>
2025-11-11 18:10:15 -0600

    feat(navbar): Agregar componente backButton

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,11 +1,20 @@
 import { useNavbar } from "../context/NavbarTitleContext";
+import BackButton from "./backButton";
 
 export default function Navbar() {
   const { title } = useNavbar();
 
   return (
-    <nav className="w-full py-6 backdrop-blur-md shadow-lg z-50">
-      <h1 className="text-center text-3xl font-semibold text-white">{title}</h1>
+    <nav className="w-full py-4 px-6 backdrop-blur-md shadow-lg z-50 flex items-center justify-between">
+      <div className="shrink-0">
+        <BackButton />
+      </div>
+
+      <h1 className="flex-1 text-center text-3xl font-semibold text-white">
+        {title}
+      </h1>
+
+      <div className="w-[100px]" />
     </nav>
   );
 }
