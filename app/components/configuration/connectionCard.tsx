9894422775ac77107a6ae9927e292bd41868adf1
cexs <cesar.serafin@outlook.com>
2025-11-11 18:09:01 -0600

    feat(connectionCard): Agregar funcionalidad para conectar mandao(ESP-32)

--- app/components/configuration/connectionCard.tsx
+++ app/components/configuration/connectionCard.tsx
@@ -1,24 +1,28 @@
+import { useState } from "react";
 import image from "~/assets/mando.png";
-
-export function ConnectionToEsp32() {
-  return alert("Conectando a ESP32...");
-}
+import ConnectionModal from "./connectionModal";
 
 export default function ConnectionCard() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className="connection-card bg-white backdrop-blur-md rounded-lg p-8 pl-15 pr-15 flex flex-col gap-y-9 items-center shadow-lg">
+    <div className="connection-card bg-[#F0F0F0] backdrop-blur-md rounded-lg p-8 flex flex-col gap-y-9 items-center shadow-lg">
       <h2 className="text-2xl font-bold text-black/90">
         ¡Inicia conexión con el mando!
       </h2>
-      <img src={image} alt="Connection Image" className="w-45 h-38" />
+
+      <img src={image} alt="Connection Image" className="w-44 h-36" />
+
       <button
-        type="submit"
-        onClick={ConnectionToEsp32}
-        className="px-6 py-2 text-1xl w-full text-white rounded-lg shadow-md hover:shadow-lg active:shadow-sm active:translate-y-1 transition-all duration-100"
+        type="button"
+        onClick={() => setIsModalOpen(true)}
+        className="px-6 py-2 hover:opacity-90 text-xl w-full text-white rounded-lg shadow-md hover:shadow-lg active:translate-y-0.5 transition-all duration-150"
         style={{ background: "var(--color-secundario)" }}
       >
         Conectar
       </button>
+
+      {isModalOpen && <ConnectionModal onClose={() => setIsModalOpen(false)} />}
     </div>
   );
 }
